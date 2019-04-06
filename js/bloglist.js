6a9be5ceab30be88879b81a4bea135093ef674d2
abhishekchitransh <abhishek.chitransh@gmail.com>
2019-04-06 15:47:30 +0530

    added confirm dialogue, added signup and signin modals.

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,6 +1,22 @@
 function deletePost(){
-    window.confirm("Are you sure you want to delete this post");
+    var deletePost= document.getElementById('deletePost');
+    deletePost.style.display="block";
+    var closeSignup = document.getElementsByClassName('close')[2];
+    closeSignup.addEventListener("click",function(){
+    deletePost.style.display="none";
+    })
+  }
+
+function confDel(value){
+    var deletePost= document.getElementById('deletePost');
+    deletePost.style.display="block";
+    var cnfNo = document.getElementsById('cnfNo');
+    var cnfYes = document.getElementsById('cnfYes');
+    cnfNo.addEventListener("click",function(){
+        deletePost.style.display="none";
+    })
 }
+
 function showPost(){
     alert("show post");
     location.href = "post.html";
